b3303e73dacbbd19928232af922b64fdce98db2a
Clockwork <alex@clockwork.gr>
2021-02-23 17:58:10 +0200

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+//import { createStore, createLogger } from 'vuex'
+import { createStore } from 'vuex'
+import init from './config'
+
+const store = createStore({
+	state() {
+		return {}
+	},
+	mutations: {},
+	actions: {},
+	chain: {}
+	//	plugins: [createLogger()]
+})
+init(store)
+export default store
